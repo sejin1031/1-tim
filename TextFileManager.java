bea5a52d3eca86da8f6eba70c68ab45adda71113
Sejin-Eom <sejin1031@naver.com>
2016-11-21 12:24:47 +0900

    Update TextFileManager.java
    add save method

--- TextFileManager.java
+++ TextFileManager.java
@@ -13,6 +13,23 @@ public class TextFileManager {
     public TextFileManager(Context context) {
         mContext = context;
     }
+    
+    public void save(String strData){
+        if(strData == null || strData.equals("")){
+            return;
+        }
+        FileOutputStream fosMemo = null;
+        
+        try{
+            //파일에 데이터를 쓰기 위해서 output 스트림 생성
+            fosMemo = mContext.openFileOutput(FILE_NAME,Context,MODE_PRIVATE);
+            //파일에 메모적기
+            fosMemo.write(strData.getBytes());
+            fosMemo.close();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
     
 }
