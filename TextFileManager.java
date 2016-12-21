28aff7965f6d07b762738703b3a39d082085ea54
dury008 <dury008@naver.com>
2016-12-21 09:06:30 +0900

    modified Textfilemanager.java

--- TextFileManager.java
+++ TextFileManager.java
@@ -6,15 +6,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.*;
 
-public class TextFileManager {
-    private static final String FILE_NAME = "LilmeMemo.txt";
+public class TextFileManager {//메모 파일을 관리해주는 객체
+    private static final String FILE_NAME = "LilmeMemo.txt"; //LilmeMemo.txt파일을 선언
     Context mContext = null;
     
     public TextFileManager(Context context) {
         mContext = context;
     }
     
-    public void save(String strData){//저장 기능
+    public void save(String strData){//메모파일을 저장해주는 함수
         if(strData == null || strData.equals("")){
             return;
         }
@@ -30,7 +30,7 @@ public class TextFileManager {
             e.printStackTrace();
         }
     }
-    public String load(){ //불러오기 기능
+    public String load(){ //메포파일을 불러오는 함수
         try{
             //produce FileInputStream that named fisMemo
             FileInputStream fisMemo = mContext.openFileInput(FILE_NAME);
@@ -39,7 +39,7 @@ public class TextFileManager {
            while(fisMemo.read(memoData) != -1){}
             
             return new String(memoData);
-        }catch(IOException e){}
+        }catch(IOException e){}//예외처리
         return "";
     }
     //File delete method
